3442992db2cfbd3442d4547e10fab28c6cedfcba
johnfwitt <johnfwitt@gmail.com>
2017-11-24 11:53:11 -0600

    Edited comments for clarity

--- iev.py
+++ iev.py
@@ -4,18 +4,18 @@
 sample = input("sample = ")
 result = 0
 
-samples = sample.split() # separate the samples into a list
+samples = sample.split() # split the sample up into a list
 
-# find the number of each parent pair type that will produce ODA by multiplying the total number of pairs by their chance of producing ODA
-# multiply that number by the number of expected offspring (2)
-# add it to the total number of ODA produced by all pair types
+# Find the number of ODA-producing parents of each type by multiplying the number of pairs by their chance of producing ODA
+# Multiply that number by the number of offspring they will produce (2)
+# Add that number to the total expected ODA of the population
 
-result += (float(samples[0]) * 1) * 2 # The first three pairs have at least one homozygous dominant parent and will always produce ODA
-result += (float(samples[1]) * 1) * 2
-result += (float(samples[2]) * 1) * 2
-result += (float(samples[3]) * .75) * 2 # Two heterozygous parents have a 75% chance of producing ODA
-result += (float(samples[4]) * .5) * 2 # One heterozygous and one homozygous recessive parent have a 50% chance of producing ODA
-# two homozygous recessive parents do not produce ODA and are not added to the result
+result += (float(samples[0]) * 1) * 2 # The first three pairs have at least one homozygous dominant parent and will always produce ODA; AA-AA
+result += (float(samples[1]) * 1) * 2 # AA-Aa
+result += (float(samples[2]) * 1) * 2 # AA-aa
+result += (float(samples[3]) * .75) * 2 # Two heterozygous parents have a 75% chance of producing ODA; Aa-Aa
+result += (float(samples[4]) * .5) * 2 # One heterozygous and one homozygous recessive parent have a 50% chance of producing ODA; Aa-aa
+# two homozygous recessive parents do not produce ODA and are not added to the result; aa-aa
 
 print (result)
 
